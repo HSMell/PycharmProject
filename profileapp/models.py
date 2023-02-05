9fb01611447939438b985a62137cdb014a3b9acf
HSMell <hyde2598@gmail.com>
2023-02-05 19:04:47 +0900

    How to use ModelForm

---
+++ profileapp/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE, related_name='profile')
+    image = models.ImageField(upload_to='profile/',null=True)         # /media/profile/* 이미지가 저장될것
+    nickname = models.CharField(max_length=20, unique=True )
+    message = models.CharField(max_length=100, null=True)
